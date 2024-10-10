c560c9c87c7996ac221dd8782a4f74d40c0be52c
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-11 01:54:34 +0600

    fix: fixing deployment issue

--- src/app/(dashboardLayout)/(adminDashboard)/admin-dashboard/admin-page/page.tsx
+++ src/app/(dashboardLayout)/(adminDashboard)/admin-dashboard/admin-page/page.tsx
@@ -1,23 +1,23 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import nexiosInstance from "@/config/nexios.config";
+// import nexiosInstance from "@/config/nexios.config";
 import AddCar from "./components/AddCar";
 
 const PetStoriesManagement = async () => {
-  const res: any = await nexiosInstance.get("/pet-stories", {
-    next: {
-      tags: ["petStoriesTable"],
-    },
-  });
+  // const res: any = await nexiosInstance.get("/pet-stories", {
+  //   next: {
+  //     tags: ["petStoriesTable"],
+  //   },
+  // });
 
   return (
     <>
       <AddCar />
 
-      {res?.data?.data?.map((car: any) => (
+      {/*  {res?.data?.data?.map((car: any) => (
         <div key={car._id}>
           <h1>{car.name}</h1>
         </div>
-      ))}
+      ))} */}
     </>
   );
 };
