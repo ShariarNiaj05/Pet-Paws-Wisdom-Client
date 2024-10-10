b9dea4b2cbd758f79c08c7c704fe72f0655ff99a
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-10 23:37:27 +0600

    deploy: fixing deployment issue

--- src/app/(dashboardLayout)/(adminDashboard)/admin-dashboard/admin-page/action.ts
+++ src/app/(dashboardLayout)/(adminDashboard)/admin-dashboard/admin-page/action.ts
@@ -29,3 +29,26 @@ export const createPetStory = async (
     throw error;
   }
 };
+
+export const createCar = async (_pre: any, formData: any): Promise<any> => {
+  try {
+    console.log(formData, "formData");
+
+    const newFormData = {
+      ...Object.fromEntries(formData),
+      rating: Number(Object.fromEntries(formData).rating),
+      passengerCapacity: Number(Object.fromEntries(formData).passengerCapacity),
+    };
+
+    const res = await nexiosInstance.post("/pet-stories", newFormData);
+
+    revalidateTag("petStoriesTable");
+
+    console.log(res.data, "res.data");
+
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
