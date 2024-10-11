c371b33f2dc9959a0ffd38a27e69cfdf21d24db1
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-11 21:22:46 +0600

    api: implementing redux

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -40,6 +40,9 @@ export const store = configureStore({
     }).concat(baseApi.middleware),
 });
 
+// Infer the type of makeStore
+export type AppStore = ReturnType<typeof store>;
+
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
