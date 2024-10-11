1db980556e6ca0094fb1e7fb20858dc998ee3f23
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-11 19:33:24 +0600

    api: implementing redux

---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { RootState } from "../store";
+
+export const baseApi = createApi({
+  reducerPath: "baseApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://pet-paws-wisdom-server.vercel.app/api/v1",
+    // baseUrl: "http://localhost:5000/api",
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth?.auth.token;
+
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  tagTypes: ["profile", "bikes", "users", "rentals"],
+  endpoints: () => ({}),
+});
