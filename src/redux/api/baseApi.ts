d1a97026ebeae33edb60ba7062575b1fc09d2115
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-11 19:39:44 +0600

    api: implementing redux

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -5,7 +5,7 @@ export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
     baseUrl: "https://pet-paws-wisdom-server.vercel.app/api/v1",
-    // baseUrl: "http://localhost:5000/api",
+    // baseUrl: "http://localhost:5000/api/v1",
     prepareHeaders: (headers, { getState }) => {
       const token = (getState() as RootState).auth?.auth.token;
 
