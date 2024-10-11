929cb144c78419160cad7ab15710d83cde8dedf6
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-11 22:50:14 +0600

    UI: implementing login

--- src/app/(commonLayout)/components/login/LoginForm.tsx
+++ src/app/(commonLayout)/components/login/LoginForm.tsx
@@ -2,13 +2,34 @@
 import { useRouter } from "next/router";
 import { useState } from "react";
 import { Button, Input, Card, Text } from "@nextui-org/react";
+import nexiosInstance from "@/config/nexios.config";
 
 const LoginForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-  const router = useRouter();
+  //   const router = useRouter();
 
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const response = await nexiosInstance.post("/login", {
+        email,
+        password,
+      });
+      console.log(response);
+      //   const { token } = response.data;
+
+      // Save token in localStorage
+      //   localStorage.setItem("token", token);
+
+      // Redirect to dashboard after successful login
+      //   router.push("/dashboard");
+    } catch (error: any) {
+      setError(error.response?.data?.message || "Login failed");
+    }
+  };
   return (
     <Card css={{ p: "$10", mw: "400px", margin: "auto", mt: "50px" }}>
       <Text h3>Login</Text>
