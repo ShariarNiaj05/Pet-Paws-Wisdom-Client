69d94e35a5518f1ca78178351d016a4833d5cc3f
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-14 23:31:39 +0600

    auth: setting user to local storage

--- src/app/(commonLayout)/components/pages/shared/UserInfoFetcher.tsx
+++ src/app/(commonLayout)/components/pages/shared/UserInfoFetcher.tsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
 import { UserInfoClient } from "./UserInfoClient";
 
 export function UserInfoFetcher({ children }: { children: React.ReactNode }) {
-  const { email, role, _id } = localStorage.getItem("userInfo");
+  const [userInfo, setUserInfo] = useState<{
+    email: string;
+    role: string;
+    _id: string;
+  } | null>(null);
+
+  useEffect(() => {
+    // Ensure localStorage is accessed only on the client side
+    const storedUserInfo = localStorage.getItem("userInfo");
+    if (storedUserInfo) {
+      const parsedUserInfo = JSON.parse(storedUserInfo);
+      setUserInfo(parsedUserInfo);
+    }
+  }, []);
+
+  if (!userInfo) {
+    return <p>Loading...</p>;
+  }
+
+  const { email, role, _id } = userInfo;
 
   return (
     <UserInfoClient email={email} role={role} _id={_id}>
