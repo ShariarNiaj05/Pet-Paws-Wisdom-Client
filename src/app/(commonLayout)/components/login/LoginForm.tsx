8e28395bf4fcd6085f7a520399727b32857e678f
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-14 23:17:26 +0600

    auth: setting user to local storage

--- src/app/(commonLayout)/components/login/LoginForm.tsx
+++ src/app/(commonLayout)/components/login/LoginForm.tsx
@@ -24,11 +24,11 @@ const LoginForm = () => {
       });
       console.log("response", response);
       const { token } = response.data;
-      // console.log("decoded token", decode(JSON.stringify(token)));
+      console.log("decoded token", JSON.stringify(decode(token)));
       // Save token in localStorage
       localStorage.setItem(
         "userInfo",
-        decode(JSON.parse(token)) as unknown as string
+        JSON.stringify(decode(token)) as unknown as string
       );
 
       // Redirect to dashboard after successful login
