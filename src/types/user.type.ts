23dcf6cf889f66d8b96a3dcdea7493d15d435f2f
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-16 00:17:37 +0600

    accessing user info

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -1,7 +1,7 @@
-type TUser = {
-  email: "user1@gmail.com";
-  exp: 1729102530;
-  iat: 1729016130;
-  role: "user";
-  _id: "6705715f71e6cba42537f1da";
+export type TUser = {
+  email: string;
+  exp?: Date;
+  iat?: Date;
+  role: string;
+  _id: string;
 };
