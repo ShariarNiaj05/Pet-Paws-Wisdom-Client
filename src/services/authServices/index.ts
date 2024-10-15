f2c8dc4bf383142da8470c73cdae533c96ba8e25
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-16 00:28:19 +0600

    accessing user info

--- src/services/authServices/index.ts
+++ src/services/authServices/index.ts
@@ -8,6 +8,9 @@ import { cookies } from "next/headers";
 export const loginUser = async (userData: TUser) => {
   try {
     const { data } = await nexiosInstance.post("/auth/login", userData);
+    if (!data) {
+      throw new Error("Nothing found");
+    }
 
     if (data.success) {
       cookies().set("accessToken", data?.data?.accessToken);
