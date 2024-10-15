5830c3e7a5778f95651328c78841976d1a2e9516
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-16 00:11:18 +0600

    accessing user info

--- src/services/authServices/index.ts
+++ src/services/authServices/index.ts
@@ -1,1 +1,16 @@
 "use server";
+
+export const loginUser = async (userData: FieldValues) => {
+  try {
+    const { data } = await axiosInstance.post("/auth/login", userData);
+
+    if (data.success) {
+      cookies().set("accessToken", data?.data?.accessToken);
+      cookies().set("refreshToken", data?.data?.refreshToken);
+    }
+
+    return data;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+};
