d09cc0913809333a93f0157570e31a58188bac89
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-16 01:28:00 +0600

    accessing user info

--- src/context/user.provider.tsx
+++ src/context/user.provider.tsx
@@ -49,6 +49,9 @@ export const useUser = () => {
     router.push("/");
   }
  */
+  if (context === undefined) {
+    throw new Error("useUser must be used within the UserProvider context");
+  }
   return context;
 };
 
