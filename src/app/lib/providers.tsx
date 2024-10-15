7ce279af8f1391b196ac5d490d168b354736638e
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-16 01:24:42 +0600

    accessing user info

--- src/app/lib/providers.tsx
+++ src/app/lib/providers.tsx
@@ -3,14 +3,15 @@ import * as React from "react";
 // 1. import `NextUIProvider` component
 import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import UserProvider from "@/context/user.provider";
+// import UserProvider from "@/context/user.provider";
 function Providers({ children }: { children: React.ReactNode }) {
   // 2. Wrap NextUIProvider at the root of your app
   return (
     <NextUIProvider>
       {" "}
       <NextThemesProvider attribute="class" defaultTheme="dark">
-        <UserProvider>{children}</UserProvider>
+        {/* <UserProvider>{children}</UserProvider> */}
+        {children}
       </NextThemesProvider>
     </NextUIProvider>
   );
