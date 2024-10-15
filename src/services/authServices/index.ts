76b401936d81dc16e32e7acfe507a7fefe3b7e56
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-16 00:43:21 +0600

    accessing user info

--- src/services/authServices/index.ts
+++ src/services/authServices/index.ts
@@ -5,6 +5,21 @@ import nexiosInstance from "@/config/nexios.config";
 import { jwtDecode } from "jwt-decode";
 import { cookies } from "next/headers";
 
+export const registerUser = async (userData: any) => {
+  try {
+    const { data } = await nexiosInstance.post("/auth/register", userData);
+
+    if (data.success) {
+      cookies().set("accessToken", data?.data?.accessToken);
+      cookies().set("refreshToken", data?.data?.refreshToken);
+    }
+
+    return data;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+};
+
 export const loginUser = async (userData: any) => {
   try {
     const { data } = await nexiosInstance.post("/auth/login", userData);
