fde21b305515775baaad764c95ab3a9725e48e3e
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-16 22:05:58 +0600

    auth options added

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { AuthOptions } from "@/config/nextauth.config";
+// import { AuthOptions } from "@/config/nextauth.config";
 import NextAuth from "next-auth";
 
 const handler = NextAuth(AuthOptions);
