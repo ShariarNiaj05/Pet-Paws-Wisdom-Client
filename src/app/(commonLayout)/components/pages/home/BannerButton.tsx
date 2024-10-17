4fd6eda30f96d512011ba4d9f3becf395235dd60
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-18 00:35:10 +0600

    ui: changing dashboard ui

--- src/app/(commonLayout)/components/pages/home/BannerButton.tsx
+++ src/app/(commonLayout)/components/pages/home/BannerButton.tsx
@@ -1,14 +1,14 @@
 "use client";
+import { useUser } from "@/context/user.provider";
 import { Button } from "@nextui-org/react";
+import Link from "next/link";
 
 const BannerButton = () => {
+  const { user } = useUser();
   return (
     <div className="space-x-4">
       <Button onClick={() => console.log("Hello")} color="primary" radius="sm">
-        See Stories
-      </Button>
-      <Button color="primary" variant="bordered" radius="sm">
-        Share Stories
+        <Link href={"/"}>See Stories</Link>
       </Button>
     </div>
   );
