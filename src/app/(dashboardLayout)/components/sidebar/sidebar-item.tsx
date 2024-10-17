b6936668e7ce22f7f3c432163842e87104623b2b
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-18 00:05:39 +0600

    ui: changing dashboard ui

--- src/app/(dashboardLayout)/components/sidebar/sidebar-item.tsx
+++ src/app/(dashboardLayout)/components/sidebar/sidebar-item.tsx
@@ -23,8 +23,8 @@ export const SidebarItem = ({ icon, title, isActive, href = "" }: Props) => {
       <div
         className={clsx(
           isActive
-            ? "bg-primary-100 [&_svg_path]:fill-primary-500"
-            : "hover:bg-default-100",
+            ? "bg-brown-300 [&_svg_path]:fill-primary-500"
+            : "hover:bg-brown-50",
           "flex gap-2 w-full min-h-[44px] h-full items-center px-3.5 rounded-xl cursor-pointer transition-all duration-150 active:scale-[0.98]"
         )}
         onClick={handleClick}
