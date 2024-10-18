e163669d280c033f60122c3a30d7f0ef9117e8dd
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-18 19:25:10 +0600

    api: adding create post ui

---
+++ src/services/contentServices/index.ts
@@ -0,0 +1,83 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use server";
+
+import nexiosInstance from "@/config/nexios.config";
+import { jwtDecode } from "jwt-decode";
+import { cookies } from "next/headers";
+
+interface ServiceResponse {
+  success: boolean;
+  token?: string;
+  message: string;
+  data: {
+    author: string;
+    title: string;
+    body: string;
+    category: string;
+    tags?: string[];
+    isPremium: boolean;
+    upvotes: number;
+    downvotes: number;
+    createdAt?: Date;
+    updatedAt?: Date;
+  };
+}
+
+/* export const registerUser = async (userData: any) => {
+  try {
+    const { data } = await nexiosInstance.post<AuthResponse>(
+      "/auth/register",
+      userData
+    );
+
+    if (data.success) {
+      cookies().set("accessToken", data?.data?.accessToken);
+      cookies().set("refreshToken", data?.data?.refreshToken);
+    }
+
+    return data;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}; */
+
+export const loginUser = async (userData: any) => {
+  try {
+    const { data } = await nexiosInstance.post<AuthResponse>(
+      "/auth/login",
+      userData
+    );
+    console.log("login user info", data);
+    if (!data) {
+      throw new Error("Nothing found");
+    }
+    console.log("user data in auth service file", data);
+    if (data.success) {
+      cookies().set("accessToken", data?.data?.accessToken);
+      cookies().set("refreshToken", data?.data?.refreshToken);
+    }
+
+    return data;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+};
+
+export const getCurrentUser = async () => {
+  const accessToken = cookies().get("accessToken")?.value;
+
+  let decodedToken = null;
+
+  if (accessToken) {
+    decodedToken = await jwtDecode(accessToken);
+
+    return {
+      _id: decodedToken._id,
+      //   name: decodedToken.name,
+      email: decodedToken.email,
+      role: decodedToken.role,
+    };
+  }
+
+  return decodedToken;
+};
