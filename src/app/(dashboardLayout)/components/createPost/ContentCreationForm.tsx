5e31df2da766339f77cbdc91107992964d34bac9
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-20 00:06:44 +0600

    feat: add post feature

--- src/app/(dashboardLayout)/components/createPost/ContentCreationForm.tsx
+++ src/app/(dashboardLayout)/components/createPost/ContentCreationForm.tsx
@@ -59,7 +59,7 @@ const ContentCreationForm = () => {
 
     fetchCategories();
   }, []);
-  console.log(allCategory);
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     console.log(body);
