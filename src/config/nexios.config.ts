9ec773321078f53ee54fc55e942eeb9622305f47
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-20 23:15:39 +0600

    feat: adding create content feature

--- src/config/nexios.config.ts
+++ src/config/nexios.config.ts
@@ -14,4 +14,19 @@ const defaultConfig: NexiosOptions = {
 
 const nexiosInstance = new Nexios(defaultConfig);
 
+nexiosInstance.interceptors.request.use((config) => {
+  const token = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith("accessToken="))
+    ?.split("=")[1];
+
+  console.log("token on nexios interceptor", token);
+  if (token) {
+    config.headers = config.headers || {};
+    config.headers["Authorization"] = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 export default nexiosInstance;
