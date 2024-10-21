cd0ce8c2a24ecea01838b43aeef169c7c920860c
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-22 01:21:12 +0600

    debug: debugging content creation

--- src/config/nexios.config.ts
+++ src/config/nexios.config.ts
@@ -6,7 +6,7 @@ const defaultConfig: NexiosOptions = {
   baseURL: "http://localhost:5000/api/v1",
   headers: {
     "Content-Type": "application/json",
-    Accept: "application/json",
+    // Accept: "application/json",
   },
   credentials: "include",
   timeout: 10000,
