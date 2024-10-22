e7d21c68ff57f8883403db7f7f7373446f5bfd26
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-23 01:12:29 +0600

    setting up axios for api call

--- src/services/getCategory.ts
+++ src/services/getCategory.ts
@@ -1,7 +1,6 @@
 import axiosSecureInstance from "@/config/axiosSecure.config";
 import nexiosInstance from "@/config/nexios.config";
 import { NexiosResponse } from "nexios-http/types/interfaces";
-import { cookies } from "next/headers";
 
 export interface ICategory {
   _id: string;
@@ -21,16 +20,13 @@ interface ICategoryResponse {
     totalPage: number;
   };
 }
-const accessToken = cookies().get("accessToken")?.value as string;
 
 export const getCategoryApi = async () => {
   /* const result = await nexiosInstance.get<NexiosResponse<ICategoryResponse[]>>(
     "/category",
     { headers: { accessToken: accessToken } }
   ); */
-  const result = await axiosSecureInstance.get("/category", {
-    headers: { accessToken: accessToken },
-  });
+  const result = await axiosSecureInstance.get("/category");
   const data = result?.data?.data;
 
   // console.log("cateogry resuult", result);
