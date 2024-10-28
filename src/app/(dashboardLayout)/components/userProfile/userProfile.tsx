cc2b0e7dc08d790bbefd8ab46a72fa7a6fbc48f2
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-28 18:44:20 +0600

    UI: User profile UI

--- src/app/(dashboardLayout)/components/userProfile/userProfile.tsx
+++ src/app/(dashboardLayout)/components/userProfile/userProfile.tsx
@@ -1,4 +1,13 @@
+"use client";
+import { useUser } from "@/context/user.provider";
+import { useState } from "react";
+
 const userProfile = () => {
+  const { user } = useUser();
+  const [userInfo, setUserInfo] = useState<any | null>(null);
+  const [profilePic, setProfilePic] = useState<File | null>(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
   return <div></div>;
 };
 
