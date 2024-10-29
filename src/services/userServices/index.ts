31a188dc63ed4940e5360ccc122f4e8c929c44aa
ShariarNiaj05 <shariarn85@gmail.com>
2024-10-29 19:24:03 +0600

    API: user profile setup

--- src/services/userServices/index.ts
+++ src/services/userServices/index.ts
@@ -2,7 +2,7 @@ import nexiosInstance from "@/config/nexios.config";
 
 export const getUserProfileApi = async (userId: string) => {
   try {
-    const response = await nexiosInstance.get(`/user/profile/${userId}`);
+    const response = await nexiosInstance.get(`/users/${userId}`);
     return response.data;
   } catch (error) {
     console.error("Error fetching user profile:", error);
